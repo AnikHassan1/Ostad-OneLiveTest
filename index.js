6996c6609d236df28591d6150a989628c24edcf9
Anik Hassan1 <ah491606@gmail.com>
2024-04-22 20:26:36 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// Define the printLanguages function
+function printLanguages(array){
+    for(ar of array){
+        console.log(ar);
+    }
+    
+}
+// Call the printLanguages function
+const languagesArray = ['PHP', 'JavaScript', 'Python'];
+// Call the printLanguages  function with the array (languagesArray) as argument
+printLanguages(languagesArray);
